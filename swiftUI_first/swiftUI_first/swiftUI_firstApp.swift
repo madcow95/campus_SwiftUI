675b2f940039cf4ef148afc08be0ba602350163d
MadCow <lobasketve@naver.com>
2024-01-15 19:13:39 +0900

    first commit

---
+++ swiftUI_first/swiftUI_first/swiftUI_firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_firstApp.swift
+//  swiftUI_first
+//
+//  Created by MadCow on 2024/1/15.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Diff()
+        }
+    }
+}
