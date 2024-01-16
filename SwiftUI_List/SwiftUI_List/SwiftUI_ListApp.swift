9899c45b63233a83163af02623af99f3218aa97d
lobaksetve@naver.com <lobasketve@naver.com>
2024-01-16 19:30:26 +0900

    Feat. List 화면 생성
    1. SwiftUI에서 사용되는 List View생성
    2. List내부에서 직접 UI를 만들면 최대 10개가 최대
    3. 전역변수로 배열을 만들어 ForEach를 활용해 UI를 만들면 계속 생성 가능

---
+++ SwiftUI_List/SwiftUI_List/SwiftUI_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListApp.swift
+//  SwiftUI_List
+//
+//  Created by MadCow on 2024/1/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
