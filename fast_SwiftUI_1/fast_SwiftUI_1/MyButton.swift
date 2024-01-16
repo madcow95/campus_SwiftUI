68c8af166688c944621955efece6bcfc58b434af
lobaksetve@naver.com <lobasketve@naver.com>
2024-01-16 18:41:55 +0900

    Feat. SwiftUI 복습
    1. VStack, HStack, Custom Button 만들어서 화면에 표시하는 방법 복습

---
+++ fast_SwiftUI_1/fast_SwiftUI_1/MyButton.swift
@@ -0,0 +1,18 @@
+//
+//  MyButton.swift
+//  fast_SwiftUI_1
+//
+//  Created by MadCow on 2024/1/16.
+//
+
+import SwiftUI
+
+struct MyButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyButton()
+}
---
+++ fast_SwiftUI_1/fast_SwiftUI_1/SimpleView2.swift
@@ -0,0 +1,18 @@
+//
+//  SimpleView2.swift
+//  fast_SwiftUI_1
+//
+//  Created by MadCow on 2024/1/16.
+//
+
+import SwiftUI
+
+struct SimpleView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimpleView2()
+}
