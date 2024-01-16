7c0107bfcf0e3c5aabeefcf67ee2fb1acd06aaef
lobaksetve@naver.com <lobasketve@naver.com>
2024-01-16 18:43:49 +0900

    Feat. SwiftUI 복습
    1. VStack, HStack, Custom Button 만들어서 화면에 표시하는 방법 복습

--- fast_SwiftUI_1/fast_SwiftUI_1/MyButton.swift
+++ fast_SwiftUI_1/fast_SwiftUI_1/MyButton.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct MyButton: View {
+    let buttonName: String
+    let buttonColor: UIColor
+    let buttonImage: Image?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            
+        } label: {
+            VStack {
+                if let image = buttonImage {
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50)
+                }
+                Text(buttonName)
+            }
+            .padding()
+            .background(Color(buttonColor))
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            .bold()
+        }
     }
 }
 
 #Preview {
-    MyButton()
+    MyButton(buttonName: "myButton", buttonColor: .blue, buttonImage: nil)
 }
--- fast_SwiftUI_1/fast_SwiftUI_1/SimpleView2.swift
+++ fast_SwiftUI_1/fast_SwiftUI_1/SimpleView2.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct SimpleView2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "pencil")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+                .padding()
+            
+            Text("Text Element 1")
+                .padding()
+                .bold()
+            
+            Text("Text Element 2")
+                .padding()
+            
+            Text("Text Element 3")
+                .padding()
+            
+            HStack {
+                MyButton(buttonName: "Button 1", buttonColor: .blue, buttonImage: nil)
+                MyButton(buttonName: "Button 2", buttonColor: .green, buttonImage: nil)
+            }
+            
+            MyButton(buttonName: "Complex Button", buttonColor: .orange, buttonImage: Image(systemName: "arrow.right.circle.fill"))
+        }
     }
 }
 
