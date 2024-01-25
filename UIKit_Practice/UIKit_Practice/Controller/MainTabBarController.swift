5ce394d7244421f6f6e2d218e01d46d5c5eb6cbb
lobaksetve@naver.com <lobasketve@naver.com>
2024-01-25 19:42:30 +0900

    Feat. Stage에 올리지 않은 파일 커밋

--- UIKit_Practice/UIKit_Practice/Controller/MainTabBarController.swift
+++ UIKit_Practice/UIKit_Practice/Controller/MainTabBarController.swift
@@ -5,4 +5,38 @@
 //  Created by MadCow on 2024/1/25.
 //
 
-import Foundation
+import UIKit
+
+class MainTabBarController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    func configureUI() {
+        collectionView.register(MainTabBarView.self, forCellWithReuseIdentifier: "MainTabBarView")
+    }
+    
+    func tabBar(unSelected: UIImage, selected: UIImage, view: UIViewController, title: String) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: view)
+        
+//        nav.tabBarItem
+        
+        return nav
+    }
+}
+
+extension MainTabBarController {
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainTabBarView", for: indexPath) as! MainTabBarView
+        
+        return cell
+    }
+}
+
+extension MainTabBarController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 80)
+    }
+}
--- UIKit_Practice/UIKit_Practice/View/MainTabBarView.swift
+++ UIKit_Practice/UIKit_Practice/View/MainTabBarView.swift
@@ -5,4 +5,25 @@
 //  Created by MadCow on 2024/1/25.
 //
 
-import Foundation
+import UIKit
+
+class MainTabBarView: UICollectionViewCell {
+    private let testLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Test"
+        label.textColor = .red
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(testLabel)
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        testLabel.topAnchor.constraint(equalTo: topAnchor, constant: 20).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
