d3e94ba6fe5080f88f708773c34a22a24dac4b64
lobaksetve@naver.com <lobasketve@naver.com>
2024-01-29 22:45:03 +0900

    Feat. Navigation Controller 생성 1. 설정 화면을 만들 Navigation Controller 생성

--- UIKit_Practice/UIKit_Practice/ViewController.swift
+++ UIKit_Practice/UIKit_Practice/ViewController.swift
@@ -11,21 +11,48 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let safeArea = view.safeAreaLayoutGuide
+        title = "First View"
         
-        let testLabel = UILabel()
-        testLabel.text = "Test Label"
-        testLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(testLabel)
-        testLabel.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 30).isActive = true
-        testLabel.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 30).isActive = true
+        let button = UIButton(type: .system)
+        button.setTitle("Next", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(nextPage), for: .touchUpInside)
+        view.addSubview(button)
         
-        let testTextField = UITextField()
-        testTextField.placeholder = "Test PlaceHolder"
-        testTextField.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(testTextField)
-        testTextField.topAnchor.constraint(equalToSystemSpacingBelow: testLabel.bottomAnchor, multiplier: 5).isActive = true
-        testTextField.leftAnchor.constraint(equalTo: testLabel.leftAnchor).isActive = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc func nextPage() {
+        navigationController?.pushViewController(NextPageController(), animated: true)
+    }
+}
+
+class NextPageController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Second View"
+        
+        let button = UIButton(type: .system)
+        button.setTitle("Second Page", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(beforePage), for: .touchUpInside)
+        view.addSubview(button)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc func beforePage() {
+        print("before page button clicked")
     }
 }
